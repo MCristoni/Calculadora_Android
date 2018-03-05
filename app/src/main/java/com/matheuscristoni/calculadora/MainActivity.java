aa2f8c64bdbb109f66b5751209f7e53162bcb19b
MCristoni <matheusmgc@globo.com>
2018-03-05 00:14:47 -0300

    Commit Teste

--- app/src/main/java/com/matheuscristoni/calculadora/MainActivity.java
+++ app/src/main/java/com/matheuscristoni/calculadora/MainActivity.java
@@ -1,13 +1,11 @@
 package com.matheuscristoni.calculadora;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -169,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
         btnMais.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Teste
                 if (txtResultado.getText().length() > 0 && operador == 5){
                     txtResultado.setText(txtResultado.getText() + "+");
                     operador = 0; //Seta operador para +
